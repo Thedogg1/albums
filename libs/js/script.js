c5b9024efa3742bde6f8a86e23ae9beb960e2ecf
TheDogg1 <terencemartin1@sky.com>
2024-06-27 13:35:41 +0100

    crud operations complete

--- libs/js/script.js
+++ libs/js/script.js
@@ -5,6 +5,27 @@ $('#editAlbumForm').on('submit', function (e) {
   e.preventDefault();
   editAlbums();
 });
+
+$('#newAlbumForm').on('submit', function (e) {
+  e.preventDefault();
+  newAlbum();
+});
+
+$('#deleteAlbumForm').on('submit', function (e) {
+  e.preventDefault();
+  deleteAlbum();
+});
+
+
+
+$('#addBtn').click(function () {
+  window.alert("bollocks");
+  
+  $('#newAlbumModal').modal('show');
+
+
+});
+
 //Get all grid data
 
 
@@ -48,8 +69,8 @@ function createAlbumTable(data) {
       '>' +
       "<i class='fa-solid fa-pencil fa-fw'></i>" +
       '</button>' +
-      "<button type='button' class='btn btn-primary btn-sm deletePersonnelBtn ms-1' data-bs-toggle='modal'" +
-      "data-bs-target='#deletePersonnelModal' data-id=" +
+      "<button type='button' class='btn btn-primary btn-sm deletealbumBtn ms-1' data-bs-toggle='modal'" +
+      "data-bs-target='#deleteAlbumModal' data-id=" +
       id +
       '>' +
       "<i class='fa-solid fa-trash fa-fw'></i></button></td></tr>";
@@ -73,7 +94,7 @@ function createAlbumTable(data) {
         albumID: $(e.relatedTarget).attr('data-id'), 
       },
       success: function (result) {
-        console.log(result);
+       
         var resultCode = result.status.code;
       
         if (resultCode == 200) {
@@ -132,4 +153,101 @@ function createAlbumTable(data) {
     });
     setTimeout(function() {$('#editAlbumModal').modal('hide');}, 3000);
   }
+
+
+  //insert an album
+
+  function newAlbum() {
+    $.ajax({
+      url: './libs/php/addAlbum.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        albumName: $('#newAlbumName').val(),
+        artistName: $('#newArtistName').val(),
+        ranking: $('#newRanking').val(),
+        year_release: $('#newReleaseYear').val(),
+        
+      },
+      success: function (result) {
+        
+        $('#newAlbumStatus').html(result.data);
+        $('#newAlbumStatus').addClass('databaseStatus');
+        setTimeout(function() {$('#newAlbumModal').modal('hide');}, 3000);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        $('#newAlbumModal .modal-title').replaceWith('Error retrieving data');
+      },
+    });
+  }
+
+
+  //delete an album
+
+  function deleteAlbum() {
+    $.ajax({
+      url: './libs/php/deleteAlbum.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        albumID: $('#deleteAlbumID').val(),
+        albumName:  $('#deleteAlbumName').val(),
+      },
+      success: function (result) {
+        $('#deleteAlbumStatus').html(result.status);
+        $('#deleteAlbumStatus').addClass('databaseStatus');
+  
+        setTimeout(function() {$('#deleteAlbumModal').modal('hide');}, 3000);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        $('#deleteAlbumModal .modal-title').replaceWith('Error retrieving data');
+      },
+    });
+  }
+  
+
+  //fill in delete modal
+
+  $('#deleteAlbumModal').on('show.bs.modal', function (e) {
+    let id = $(e.relatedTarget).attr('data-id');
+    $('#deleteAlbumID').val(id);
+   
+    $('#deleteAlbumStatus').html('');
+  
+  
+  let deleteText;
+  
+    let albumID = $('#deleteAlbumID').val();
+  
+    $.ajax({
+      url: './libs/php/getAlbumsByID.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        albumID: albumID,
+      },
+  
+      success: function (result) {
+        console.log(result.data[0].albumName);
+        var resultCode = result.status.code;   
+        if (resultCode == 200) {
+          $('#deleteAlbumName').val(result.data[0].albumName);      
+          // let saveButton = document.getElementById('btnDeleteAlbum');    
+          // let closeButton = document.getElementById('btnCloseDeleteAlbum');  
+              
+          
+            deleteText="You are about to permanently delete <b>"+ result.data[0].albumName +"</b> from the system. Are you sure you want to proceed?"
+            //saveButton.hidden = false;
+           // closeButton.innerText="NO";
+          
+          $('#delAlbumText').html(deleteText);
+          
+        } 
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        $('#deleteAlbumModal .modal-title').replaceWith('Error retrieving data');
+      },
+    });
+  
+  });
   
